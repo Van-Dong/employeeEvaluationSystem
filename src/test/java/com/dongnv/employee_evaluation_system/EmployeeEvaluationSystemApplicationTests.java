5a5f094a712f4435a7b1acc86b77e2d6766cfeb3
Nguyen Van Dong <DongNV@vietis.vn>
2024-10-02 16:15:57 +0700

    init project

---
+++ src/test/java/com/dongnv/employee_evaluation_system/EmployeeEvaluationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongnv.employee_evaluation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeEvaluationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
