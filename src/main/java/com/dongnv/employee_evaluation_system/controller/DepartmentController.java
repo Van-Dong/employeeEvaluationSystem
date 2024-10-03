97c65680497a62fd11712da5679c63e23ed4797d
Nguyen Van Dong <DongNV@vietis.vn>
2024-10-03 17:50:02 +0700

    fix bug

--- src/main/java/com/dongnv/employee_evaluation_system/controller/DepartmentController.java
+++ src/main/java/com/dongnv/employee_evaluation_system/controller/DepartmentController.java
@@ -11,9 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.util.List;
@@ -48,6 +46,12 @@ public class DepartmentController {
             return "department/add-department";
         }
         departmentService.save(department);
+        return "redirect:/department";
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteDepartment(@PathVariable long id) {
+        departmentService.deleteById(id);
         return "redirect:/department";
     }
 }
